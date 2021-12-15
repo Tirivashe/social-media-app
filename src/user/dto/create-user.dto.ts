499b68edfed4a935abab1a165f555001146d413a
Tiri <shaymusts@gmail.com>
2021-12-16 01:20:45 +0200

    Defined the dto to create a user with validation

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,31 @@
-export class CreateUserDto {}
+import { IsAlpha, IsAlphanumeric, IsEmail, IsNotEmpty, IsNumber, IsString } from "class-validator"
+
+export class CreateUserDto {
+  @IsAlpha()
+  @IsString()
+  @IsNotEmpty()
+  firstName: string
+
+  @IsAlpha()
+  @IsString()
+  @IsNotEmpty()
+  lastName: string
+
+  @IsNumber()
+  @IsNotEmpty()
+  age: number
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string
+
+  @IsAlphanumeric()
+  @IsString()
+  @IsNotEmpty()
+  username: string
+
+  @IsAlphanumeric()
+  @IsString()
+  @IsNotEmpty()
+  password: string
+}
