61f94ae100beb4cfc60db08ce6fdcfed1cd58612
Tiri <shaymusts@gmail.com>
2021-12-16 20:21:12 +0200

    auth service promise rejection handling and response with insensitive data on validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
 export class AuthService {
@@ -8,16 +9,21 @@ export class AuthService {
   constructor(private readonly userService: UserService, private readonly jwtService: JwtService){}
 
   async validate( username: string, pass: string){
-    const user = await this.userService.findOne(username)
-    if(user && user.password === pass){
-      const { password, ...rest } = user
-      return rest
+    try {
+      const user = await this.userService.findOne(username)
+      const passwordMatch = await bcrypt.compare(pass, user?.password)
+      if(user && passwordMatch){
+        const { id,firstName, lastName, age, username, profile_pic, cover_pic, followers, isAdmin } = user
+        
+        return { id, firstName, lastName, age, username, profile_pic, cover_pic, followers, isAdmin }
+      }
+    } catch (error) {
+      return null
     }
-    return null
   }
 
   async login(user: any){
-    const payload = { sub: user.dataValues.id, username: user.dataValues.username }
+    const payload = { sub: user.id, username: user.username }
     return {
       access_token: this.jwtService.sign(payload)
     }
